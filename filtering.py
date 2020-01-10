6bbd9192645b1aadb68cd80dd324c03ded791136
Hans Yang <hyang6@stuy.edu>
2020-01-10 13:55:55 -0500

    removed a file that didn't make sense

--- filtering.py
+++
@@ -1,9 +0,0 @@
-from classes import Student
-from classes import Tutor
-
-# matching will work:
-# once parsing is done, we will have all of the things so pick a student to match
-# then run through list of tutors that are avaliable at that time. 
-
-
-# some issues with consistency.. i think that matching might need to pull from some kind of master database... or else if a tutor is amtched w multiple students or smth???
